692278f828cc7c78f5aa7e971fcb4e2d127fbb13
mallusmane <mallusmaenmaski@gmail.com>
2022-09-16 18:02:26 +0530

    feature1 commit

--- cat.java
+++ cat.java
@@ -9,13 +9,13 @@ class GFG {
     {
  
         // g is to ensures that numbers are float DATA TYPE
-        float g1 = 1.5f;
-        float g2 = 2.0f;
+        float f1 = 5656545f;
+        float f2 = 215313545f;
  
         // to store the multiplied value
         float p = f1 * f2;
  
-        // to print the devops ecplice
+        // to print the devops ecplice to the neW
         System.out.println("The product is: " + p);
     }
 }
