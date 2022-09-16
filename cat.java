4b8a2c2cb8dbbae7338b96c431d96af938a3b535
mallusmane <mallusmaenmaski@gmail.com>
2022-09-16 17:56:32 +0530

    N WITH '#' wil be ignored, and an empty message aborts the commit.

---
+++ cat.java
@@ -0,0 +1,21 @@
+
+// Java Program to print Multiplication of two floating
+// point Number.
+ 
+import java.io.*;
+ 
+class GFG {
+    public static void main(String[] args)
+    {
+ 
+        // g is to ensures that numbers are float DATA TYPE
+        float g1 = 1.5f;
+        float g2 = 2.0f;
+ 
+        // to store the multiplied value
+        float p = f1 * f2;
+ 
+        // to print the devops ecplice
+        System.out.println("The product is: " + p);
+    }
+}
